abbcb555ae996d3d202400c2dd93ac30193f89ab
flockeyrocks <78123443+flockeyrocks@users.noreply.github.com>
2021-02-08 12:38:58 -0800

    Update mux_2x1_simple.v

--- mux_2x1_simple.v
+++ mux_2x1_simple.v
@@ -29,9 +29,9 @@ module mux_4x1_3bit(
     
     wire w1_2,w1_1,w1_0,w2_2,w2_1,w2_0;                             //Interconnects between 2x1 MUXES
     
-    mux_2x1_3bit mux0 (X2,X1,X0,Y2,Y1,Y0,S0,w1_2,w1_1,w1_0);        //First 2x1 MUX for X and Y. Uses S1 for first stage
-    mux_2x1_3bit mux1 (Z2,Z1,Z0,W2,W1,W0,S0,w2_2,w2_1,w2_0);        //Second 2x1 MUX for Z and W. Uses S1 for first stage
-    mux_2x1_3bit mux2 (w1_2,w1_1,w1_0,w2_2,w2_1,w2_0,S1,M2,M1,M0);  //Third 2x1 MUX for results of first stage. Uses S0
+    mux_2x1_3bit mux0 (X2,X1,X0,Y2,Y1,Y0,S0,w1_2,w1_1,w1_0);        //First 2x1 MUX for X and Y. Uses S0 for first stage
+    mux_2x1_3bit mux1 (Z2,Z1,Z0,W2,W1,W0,S0,w2_2,w2_1,w2_0);        //Second 2x1 MUX for Z and W. Uses S0 for first stage
+    mux_2x1_3bit mux2 (w1_2,w1_1,w1_0,w2_2,w2_1,w2_0,S1,M2,M1,M0);  //Third 2x1 MUX for results of first stage. Uses S1
     
     assign S1LED = S1;
     assign S0LED = S0;
